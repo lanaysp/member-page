d378007f8485010cc38eab5f7467e34196f8a44e
Maulana Yusup <lanaysps@gmail.com>
2022-02-19 22:34:30 +0700

    remove un use variable

--- src/pages/Joined.js
+++ src/pages/Joined.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-
-import { Link } from "react-router-dom";
-
 import courses from "constants/api/courses";
 
 import ServerError from "pages/500";
@@ -35,18 +32,18 @@ export default function Joined({ history, match }) {
   if (state.isError) return <ServerError></ServerError>;
 
   return (
-    <section className="h-screen flex flex-col items-center mt-24">
+    <section className="flex flex-col items-center h-screen mt-24">
       <img
         src={`${process.env.PUBLIC_URL}/assets/images/illustration-joined.jpg`}
         alt="Success join class"
       />
-      <h1 className="text-3xl text-gray-900 mt-12">Welcome to Class</h1>
-      <p className="text-lg text-gray-600 mt-4 mtb-8 lg:w-4/12 xl:w-3/12 mx-auto text-center">
+      <h1 className="mt-12 text-3xl text-gray-900">Welcome to Class</h1>
+      <p className="mx-auto mt-4 text-lg text-center text-gray-600 mtb-8 lg:w-4/12 xl:w-3/12">
         You have successfully joined our{" "}
         <strong>{state?.data?.name ?? "Class Name"}</strong> class
       </p>
       <Link
-        className="cursor-pointer bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-5"
+        className="px-6 py-3 mt-5 text-white transition-all duration-200 bg-orange-500 shadow-inner cursor-pointer hover:bg-orange-400 focus:outline-none"
         to={`/courses/${match.params.class}`}
       >
         Start learn
