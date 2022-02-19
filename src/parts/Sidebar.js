b73bb5079ae1c0ab135cfbd5a6e649b439d2ade9
Maulana Yusup <lanaysps@gmail.com>
2022-02-19 23:10:55 +0700

    remove

--- src/parts/Sidebar.js
+++ src/parts/Sidebar.js
@@ -39,7 +39,7 @@ function Sidebar({ match, history }) {
         ></button>
       </div>
       <aside
-        className="transition-all duration-300 bg-indigo-1000 max-h-screen h-screen overflow-y-auto min-h-full fixed sm:relative z-50"
+        className="fixed z-50 h-screen max-h-screen min-h-full overflow-y-auto transition-all duration-300 bg-indigo-1000 sm:relative"
         style={sidebarStyle}
       >
         {toggleMenu && (
@@ -49,31 +49,30 @@ function Sidebar({ match, history }) {
           ></div>
         )}
         <div
-          className="max-h-screen h-screen fixed bg-indig-1000 flex flex-col content-between z-50"
+          className="fixed z-50 flex flex-col content-between h-screen max-h-screen bg-indig-1000"
           style={{ width: 280 }}
         >
-          <div className="flex flex-col text-center mt-8">
-            <div className="border border-indigo-500 rounded-full mx-auto p-2 inline-flex  mb-3">
-              <div className="rounded-full overflow-hidden">
+          <div className="flex flex-col mt-8 text-center">
+            <div className="inline-flex p-2 mx-auto mb-3 border border-indigo-500 rounded-full">
+              <div className="overflow-hidden rounded-full">
                 {USERS?.avatar ? (
                   <img
                     className="object-cover w-24 h-24"
                     src={USERS?.avatar}
-                    alt={USERS?.name}
                   />
                 ) : (
-                  <DefaultUser className="fill-indigo-500 w-24 h-24"></DefaultUser>
+                  <DefaultUser className="w-24 h-24 fill-indigo-500"></DefaultUser>
                 )}
               </div>
             </div>
 
-            <h6 className="text-white text-xl">{USERS?.name ?? "Username"}</h6>
-            <span className="text-indigo-500 text-sm">
+            <h6 className="text-xl text-white">{USERS?.name ?? "Username"}</h6>
+            <span className="text-sm text-indigo-500">
               {USERS?.profession ?? "Profession"}
             </span>
           </div>
 
-          <ul className="main-menu mt-12">
+          <ul className="mt-12 main-menu">
             <li>
               <Link
                 className={[
@@ -123,7 +122,7 @@ function Sidebar({ match, history }) {
 
           <div className="my-auto"></div>
 
-          <ul className="main-menu mt-12">
+          <ul className="mt-12 main-menu">
             <li>
               <button
                 className={[
